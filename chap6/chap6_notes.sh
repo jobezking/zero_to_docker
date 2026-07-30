4657e8e23d43e43def77f243faaf311544533972
Job King <jobezking@gmail.com>
2026-07-30 16:49:09 -0400

    push

---
+++ chap6/chap6_notes.sh
@@ -0,0 +1,7 @@
+https://hub.docker.com/
+images: build-time
+containers: run-time. Should run a single application or microservice. Use Linux kernel of host OS
+local repository: /var/lib/docker where Docker stores images, containers, and other data
+registry: a service that stores Docker images. Docker Hub is the default registry. You can also run your own private registry.
+docker images
+docker pull redis
