18889505f5a871c72e3353effa95bdfea6d6c68f
Job King <jobezking@gmail.com>
2026-07-30 15:24:27 -0400

    push

--- chap3/chap3_notes.sh
+++ chap3/chap3_notes.sh
@@ -4,3 +4,6 @@ docker pull nginx:latest
 docker run --name test -d -p 8080:80 nginx:latest #start a new container named test in detached mode and map port 8080 on the host to port 80 in the container
 docker ps #list running containers
 docker ps -a #list all containers, including stopped ones
+docker exec -it test bash
+docker stop test #stop the container named test
+docker rm test #remove the container named test
