7ce379709f4fb37647240e911f1e5a7545d0cf49
Job King <jobezking@gmail.com>
2026-07-30 15:11:58 -0400

    update

--- chap3/chap3_notes.sh
+++ chap3/chap3_notes.sh
@@ -1,1 +1,6 @@
 docker version
+docker images
+docker pull nginx:latest
+docker run --name test -d -p 8080:80 nginx:latest #start a new container named test in detached mode and map port 8080 on the host to port 80 in the container
+docker ps #list running containers
+docker ps -a #list all containers, including stopped ones
