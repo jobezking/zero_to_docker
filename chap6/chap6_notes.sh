bca23b80e08bb7480d162edda89d06e47598c3b1
Job King <jobezking@gmail.com>
2026-07-30 16:58:09 -0400

    push

--- chap6/chap6_notes.sh
+++ chap6/chap6_notes.sh
@@ -1,4 +1,4 @@
-https://hub.docker.com/
+https://hub.docker.com/ # registry docker pulls from by default though you can build your own registry
 images: build-time
 containers: run-time. Should run a single application or microservice. Use Linux kernel of host OS
 local repository: /var/lib/docker where Docker stores images, containers, and other data
