c0e581ce616b8376012fc4a6bb3bf79f820fc8b0
Job King <jobezking@gmail.com>
2026-07-30 16:40:42 -0400

    finish chap5

--- chap5/chap5_notes.sh
+++ chap5/chap5_notes.sh
@@ -2,3 +2,11 @@ docker run -d --name ctr1 nginx
 docker ps
 docker ps -a
 docker rm ctr1 -f
+
+/usr/bin/dockerd # the docker daemon
+/usr/bin/docker # the docker client
+/usr/bin/docker-init # the docker init binary
+/usr/bin/containerd # the containerd daemon
+/usr/bin/docker-containerd # the containerd daemon
+/usr/bin/docker-containerd-shim # the containerd shim
+/usr/bin/runc # the runc binary
