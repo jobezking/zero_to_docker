f0598b6c82c6cfcc7614f390463d349031f38b65
Job King <jobezking@gmail.com>
2026-07-30 15:50:32 -0400

    done chapter 3 and 4

--- chap3/chap3_notes.sh
+++ chap3/chap3_notes.sh
@@ -7,3 +7,7 @@ docker ps -a #list all containers, including stopped ones
 docker exec -it test bash
 docker stop test #stop the container named test
 docker rm test #remove the container named test
+docker build -t test:latest . # build a new image named test:latest from the Dockerfile in the current directory
+docker run -d --name web1 --publish 8080:8080 test:latest # start a new container named web1 in detached mode and map port 8080 on the host to port 8080 in the container
+docker rm web1 -f # force remove the container named web1
+docker rmi test:latest # remove the image named test:latest
