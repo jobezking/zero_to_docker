f05073ea0097a2ff99690b789145aad226606b6b
Job King <jobezking@gmail.com>
2026-07-31 11:38:54 -0400

    push

---
+++ chap8/chap8_notes.sh
@@ -0,0 +1,7 @@
+5 steps to containerize an app
+1. Write the app and create dependencies list
+2. Create Dockerfile that tells Docker how to build and run the app
+3. Build app into image
+4. Push image to registry (optional)
+5. Run container from image
+
