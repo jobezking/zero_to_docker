629771928b8d61a1bbe9ae469939479382af0df1
Job King <jobezking@gmail.com>
2026-07-31 01:43:47 -0400

    push

---
+++ chap7/chap7_notes.sh
@@ -0,0 +1,16 @@
+sudo systemctl status docker.service
+docker run <arguments> <image> <command>
+There are three ways you can tell Docker how to start an app in a container: 
+    An Entrypoint instruction in the image 
+    A Cmd instruction in the image 
+    A CLI argument
+docker run -d --name apache-server -p 8080:80 httpd:latest  # run a container in detached mode, name it apache-server, 
+                                                            # map port 8080 on host to port 80 in container, use the httpd:latest image
+docker run --rm -d alpine sleep 60 #
+docker images # list all images on the host
+docker ps # list all running containers
+
+docker exec -it webserver sh
+docker exec <container> <command>
+docker exec apache-server ls  # sends ls command to apache-server container
+docker inspect apache-server
