53efcc54e04b32b1fe40c35c066e560c69057b64
Job King <jobezking@gmail.com>
2026-08-02 23:46:54 -0400

    push

--- chap13/chap13_notes.sh
+++ chap13/chap13_notes.sh
@@ -1,4 +1,11 @@
  docker network ls
  docker network inspect bridge
+ docker network inspect bridge | grep bridge.name
+ brctl show
+ ip link show docker0
  docker network inspect host
  docker network inspect none
+ docker run -d --name c1 --network localnet alpine sleep 1d
+ docker network prune
+ docker network create --driver bridge localnet
+ docker network rm
