a767d23c07e0f3ad4f7bafa131d015eb9cdebfb2
Job King <jobezking@gmail.com>
2026-08-03 22:32:31 -0400

    push

--- chap8/chap15/chap15_notes.sh
+++ chap8/chap15/chap15_notes.sh
@@ -3,3 +3,7 @@ docker volume ls
 sudo ls -l /var/lib/docker/volumes
 sudo ls -l /var/lib/docker/volumes/myvol/_data
 docker volume inspect myvol
+docker volume prune --all # deletes all unmounted volumes
+docker volume rm myvol  # deletes myvol if it is unmounted/unused
+docker run -it --name voltainer --mount source=bizvol,target=/vol alpine  #creates container voltainer and volume bizvol and mounts it to /vol in the container
+# If you specify a volume that already exists, Docker will use it. If you specify a volume that does not exist, Docker will create it
