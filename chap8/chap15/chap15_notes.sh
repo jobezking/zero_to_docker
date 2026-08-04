df56f81f8637906c372c709962572c0051912dea
Job King <jobezking@gmail.com>
2026-08-03 22:37:20 -0400

    push

--- chap8/chap15/chap15_notes.sh
+++ chap8/chap15/chap15_notes.sh
@@ -7,3 +7,12 @@ docker volume prune --all # deletes all unmounted volumes
 docker volume rm myvol  # deletes myvol if it is unmounted/unused
 docker run -it --name voltainer --mount source=bizvol,target=/vol alpine  #creates container voltainer and volume bizvol and mounts it to /vol in the container
 # If you specify a volume that already exists, Docker will use it. If you specify a volume that does not exist, Docker will create it
+#Previous command takes you to container shell. CRTL + P + Q to exit the container shell and return to host shell.
+
+docker exec -it voltainer sh
+echo "I promise to write a book review on Amazon" > /vol/file1
+cat /vol/file1
+exit
+docker rm voltainer -f
+docker ps -a
+docker volume ls
